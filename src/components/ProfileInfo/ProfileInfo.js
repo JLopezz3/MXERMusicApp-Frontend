e8dccad6b007c12be317af0842614ba07c69f284
JLopezz3 <lopezjerel@gmail.com>
2021-01-04 22:44:54 -0600

    create ProfileInfo

---
+++ src/components/ProfileInfo/ProfileInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProfileInfo.css';
+
+export default function ProfileInfo() {
+    return (
+        <div>
+            <p>THIS IS THE PROFILE INFO PAGE, this is what comes after signup</p>
+        </div>
+    )
+}
+
+
