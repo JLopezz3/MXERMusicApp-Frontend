db2dc36a19c3ec887735aedfea8f66a8fada6775
JLopezz3 <lopezjerel@gmail.com>
2021-01-10 15:05:51 -0600

    Dashboard and Dashboard Header

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -2,10 +2,21 @@ import React from "react";
 import GetStartedCarousel from "../../components/GetStarted/GetStartedCarousel";
 import Swipe from "../../components/Swipe/Swipe";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  top: {
+    width: "100%",
+    background: "red"
+  }
+}));
+
 export default function Dashboard() {
+  const classes = useStyles();
+
   return (
     <div>
-      <p>WELCOME TO MXER! CLICK HERE TO LEARN MORE.</p>
+      <p className={classes.top} >WELCOME TO MXER! CLICK HERE TO LEARN MORE.</p>
       <GetStartedCarousel />
       <Swipe />
     </div>
